f56480ad0fa5b375b46d9180079417c080976cc7
Calvin Kopitz <calvinjk@umich.edu>
2016-11-30 19:34:49 -0500

    double to single quotes

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def say(s):
 	requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "b24ce747ab0015068a61460d85","text": s})
 
 @app.route('/', methods = ['GET', 'POST'])
-if request.method == "GET":
+if request.method == 'GET':
 	def hello_world():
 		print 'viewed'
 		return 'This is the Test bot page.'
