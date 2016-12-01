b2b92c40abc600a12d7563b851ff58b40bb3f664
Calvin Kopitz <calvinjk@umich.edu>
2016-11-30 21:31:46 -0500

    Bot ID for motherfuckers LIVE

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 print 'run'
 
 def say(s):
-	requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "088ef849ec340699ee3cbeabb8","text": s})
+	requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "2105543276296e8bd2fec9c082","text": s})
 
 @app.route('/', methods = ['GET', 'POST'])
 def masterRunEverything():
