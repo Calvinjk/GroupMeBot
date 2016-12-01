f336879fa2f432849f3027ae2752cd08408d526c
Calvin Kopitz <calvinjk@umich.edu>
2016-11-30 19:45:50 -0500

    moment of truth?

--- app.py
+++ app.py
@@ -14,19 +14,19 @@ def masterRunEverything():
 		print 'viewed'
 		return 'This is the Test bot page.'
 	else:
-		return 'Panic'
-		# def respond():
-		# message = request.get_json(force=True)
-		# if message['name']!='Troll Bot':
+		def respond():
+		message = request.get_json(force=True)
+		if message['name']!='Troll Bot':
 
-		# 	me=re.compile("\\b\\w+er\\b")
-		# 	m=me.findall(message['text'])
-		# 	for x in m:
-		# 		if len(x)>3:
-		# 			say("{0} her? I hardly know her!".format(x[0:-2]))
+			me=re.compile("\\b\\w+er\\b")
+			m=me.findall(message['text'])
+			for x in m:
+				if len(x)>3:
+					say("{0} her? I hardly know her!".format(x[0:-2]))
 
-		# 	elif 'harambe' in message['text'].lower():
-		# 		say("DICKSOUT")
+			elif 'harambe' in message['text'].lower():
+				say("DICKSOUT")
+		return 'Fuckin test bot.'
 
 
 
