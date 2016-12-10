39c01a10b0650a4431d0bff6f0025f7dc747426d
Calvin Kopitz <calvinjk@umich.edu>
2016-12-10 14:45:05 -0500

    Bot id is now testbot in testBranch

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ print 'run'
 
 def say(s):
 	# USE THIS LINE FOR LIVE
-	requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "2105543276296e8bd2fec9c082","text": s})
+	requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "088ef849ec340699ee3cbeabb8","text": s})
 	
 	# USE THIS LINE FOR DEVELOPMENT 
 	# requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "088ef849ec340699ee3cbeabb8","text": s})
