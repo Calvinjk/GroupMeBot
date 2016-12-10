49795c5f70e11374bcf38ad9231e40b463a3a65c
Calvin Kopitz <calvinjk@umich.edu>
2016-12-10 16:15:06 -0500

    still an idiot.   extra paren...

--- helperFunctions.py
+++ helperFunctions.py
@@ -12,10 +12,10 @@ def KnowHer(wordList):
 	first = wordList[0]
 			
 	if len(last) > 3 and last[-1] == 'r' and last[-2] == 'e':
-		return "{0} 'er? I hardly know her!".format(last[0:-2]))
+		return "{0} 'er? I hardly know her!".format(last[0:-2])
 
 	elif len(first) > 3 and first[-1] == 'r' and first[-2] == 'e':
-		return "{0} 'er? I hardly know her!".format(first[0:-2]))
+		return "{0} 'er? I hardly know her!".format(first[0:-2])
 	
 	return "-0"
 
