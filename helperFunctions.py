de4f44a7fea514090fea8514b811afbdae92cf22
Monica Curry <mmcurry@umich.edu>
2016-12-10 15:11:23 -0500

    say function

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,4 +1,4 @@
-def say(s):
+def say2(s):
 	# USE THIS LINE FOR LIVE
 	requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "088ef849ec340699ee3cbeabb8","text": s})
 	
@@ -8,7 +8,7 @@ def say(s):
 
 def Harambe(message):
 	if 'harambe' in message:
-		say("DICKS OUT")
+		say2("DICKS OUT")
 
 
 
