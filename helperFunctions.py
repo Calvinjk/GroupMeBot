cf94bf32d8c7732b4a72fc1f7a017654be67da18
Calvin Kopitz <calvinjk@umich.edu>
2016-12-10 16:06:34 -0500

    jank fix for KnowHer

--- helperFunctions.py
+++ helperFunctions.py
@@ -10,10 +10,12 @@ def KnowHer(message):
 	first = wordList[0]
 			
 	if len(last) > 3 and last[-1] == 'r' and last[-2] == 'e':
-		return "{0} 'er? I hardly know her!".format(last[0:-2]))
+		s = "{0} 'er? I hardly know her!".format(last[0:-2]))
+		return s
 
 	elif len(first) > 3 and first[-1] == 'r' and first[-2] == 'e':
-		return "{0} 'er? I hardly know her!".format(first[0:-2]))
+		s = "{0} 'er? I hardly know her!".format(first[0:-2]))
+		return s
 	
 	return "-0"
 
