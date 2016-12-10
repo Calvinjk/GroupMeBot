16bc807a84da33acde9669fb213e15b1360e39d7
Calvin Kopitz <calvinjk@umich.edu>
2016-12-10 16:09:08 -0500

    I am an idiot

--- helperFunctions.py
+++ helperFunctions.py
@@ -5,17 +5,15 @@ def Harambe(message):
 
 #######################################
 
-def KnowHer(message):
+def KnowHer(wordList):
 	last = wordList[-1]
 	first = wordList[0]
 			
 	if len(last) > 3 and last[-1] == 'r' and last[-2] == 'e':
-		s = "{0} 'er? I hardly know her!".format(last[0:-2]))
-		return s
+		return "{0} 'er? I hardly know her!".format(last[0:-2]))
 
 	elif len(first) > 3 and first[-1] == 'r' and first[-2] == 'e':
-		s = "{0} 'er? I hardly know her!".format(first[0:-2]))
-		return s
+		return "{0} 'er? I hardly know her!".format(first[0:-2]))
 	
 	return "-0"
 
