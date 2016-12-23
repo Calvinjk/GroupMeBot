9d289d948d13cf3df4e42650794dfbdd81230043
Calvin Kopitz <calvinjk@umich.edu>
2016-12-23 18:41:43 -0500

    TestBranch botit switched for testing

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 def say(s):
 	if (s != "-0"):
 		# USE THIS LINE FOR DEVELOPMENT
-		#requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "088ef849ec340699ee3cbeabb8","text": s})
+		requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "088ef849ec340699ee3cbeabb8","text": s})
 	
 		# USE THIS LINE FOR LIVE
-	  requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "2105543276296e8bd2fec9c082","text": s})
+	  	#requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "2105543276296e8bd2fec9c082","text": s})
 
 @app.route('/', methods = ['GET', 'POST'])
 def masterRunEverything():
@@ -25,7 +25,7 @@ def masterRunEverything():
 		wordList = re.sub("[^\w]", " ", lowerCaseMessage).split()
 
 		# Make sure bot doesnt respond to own messages
-		if message['name']!='Heroku "The Bot"':
+		if message['name'] != 'Heroku "The Bot"':
 
 			# Know her?
 			say(KnowHer(wordList))
