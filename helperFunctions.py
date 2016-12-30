92ccdf3127f1a247e5bd69e29fd29d7321ff2f20
Calvin Kopitz <calvinjk@umich.edu>
2016-12-30 14:45:52 -0500

    Added tyreePics

--- helperFunctions.py
+++ helperFunctions.py
@@ -119,6 +119,94 @@ def PostMariaPic():
 
 	return 'https://s' + sNum + '.postimg.org/' + picSuffix
 
+############### Post Tyree Pic ###############	
+
+def PostTyreePic():
+	randNum = random.randrange(0, 40)
+	sNum = '20'
+
+	if randNum == 0:
+		picSuffix = 'ism5qphgd/tyree_1.jpg'
+	if randNum == 1:
+		picSuffix = 'm0qn3r3q5/tyree_2.jpg'
+	if randNum == 2:
+		picSuffix = 'iv61djl3x/tyree_3.jpg'
+	if randNum == 3:
+		picSuffix = '4q08bqc2l/tyree_4.jpg'
+	if randNum == 4:
+		picSuffix = 'yj78kc0pp/tyree_5.jpg'
+	if randNum == 5:
+		picSuffix = '55bi4qxzx/tyree_6.jpg'
+	if randNum == 6:
+		picSuffix = 'giy1fy8il/tyree_7.jpg'
+	if randNum == 7:
+		picSuffix = 'fhxsqtrj1/tyree_8.jpg'
+	if randNum == 8:
+		picSuffix = 'jfl2g8ecd/tyree_9.jpg'
+	if randNum == 9:
+		picSuffix = 'e5g3oxu3h/tyree_10.jpg'
+	if randNum == 10:
+		picSuffix = 'e6umw3lb1/tyree_11.jpg'
+	if randNum == 11:
+		picSuffix = 'hez4957kt/tyree_12.jpg'
+	if randNum == 12:
+		picSuffix = 'ukemf91gd/tyree_13.jpg'
+	if randNum == 13:
+		picSuffix = 'g2hf79a59/tyree_14.jpg'
+	if randNum == 14:
+		picSuffix = 'f1h6i4t5p/tyree_15.jpg'
+	if randNum == 15:
+		picSuffix = 'vdr87v7h9/tyree_16.jpg'
+	if randNum == 16:
+		picSuffix = '6907ug80t/tyree_17.jpg'
+	if randNum == 17:
+		picSuffix = 'blp28kvx9/tyree_18.jpg'
+	if randNum == 18:
+		picSuffix = 'e4ar99hnh/tyree_19.jpg'
+	if randNum == 19:
+		picSuffix = 'd3aik50nx/tyree_20.jpg'
+	if randNum == 20:
+		picSuffix = 'v7i4iinq5/tyree_21.jpg'
+	if randNum == 21:
+		picSuffix = 'mdr81f0rh/tyree_22.jpg'
+	if randNum == 22:
+		picSuffix = 'ukj7szqu5/tyree_23.jpg'
+	if randNum == 23:
+		picSuffix = 'gfder6hst/tyree_24.jpg'
+	if randNum == 24:
+		picSuffix = 'rg8jw7a1p/tyree_25.jpg'
+	if randNum == 25:
+		picSuffix = 'eq4bj423h/tyree_26.jpg'
+	if randNum == 26:
+		picSuffix = '5jm0vtwv1/tyree_27.jpg'
+	if randNum == 27:
+		picSuffix = 'vgfp8g0il/tyree_28.jpg'
+	if randNum == 28:
+		picSuffix = 'or95sff6l/tyree_29.jpg'
+	if randNum == 29:
+		picSuffix = 'ev82ss9el/tyree_30.jpg'
+	if randNum == 30:
+		picSuffix = 'h16z1128t/tyree_31.jpg'
+	if randNum == 31:
+		picSuffix = 'ysilfhhnh/tyree_32.jpg'
+	if randNum == 32:
+		picSuffix = '3mtw4zvkt/tyree_33.jpg'
+	if randNum == 33:
+		picSuffix = '6i6zbuzkt/tyree_34.jpg'
+	if randNum == 34:
+		picSuffix = 'xhau70m1p/tyree_35.jpg'
+	if randNum == 35:
+		picSuffix = 'c8x5pl7kt/tyree_36.jpg'
+	if randNum == 36:
+		picSuffix = '5jqm9km8t/tyree_37.jpg'
+	if randNum == 37:
+		picSuffix = 'esssjov4t/tyree_38.jpg'
+	if randNum == 38:
+		picSuffix = 'wk4ey5ajh/tyree_39.jpg'
+	if randNum == 39:
+		picSuffix = 'gzx17m0f1/tyree_40.jpg'
+
+	return 'https://s' + sNum + '.postimg.org/' + picSuffix
 
 ############### People Pics ###############
 
@@ -129,6 +217,7 @@ def PeoplePics(wordList):
 	# Switches so we only post one pic per person
 	joePosted = "false"
 	mariaPosted = "false"
+	tyreePosted = "false"
 
 	for word in wordList:
 		# Joe
@@ -140,5 +229,10 @@ def PeoplePics(wordList):
 		if (word == "maria" or word == "mc") and mariaPosted == "false":
 			mariaPosted = "true"
 			returnURLS.append(PostMariaPic())
+
+		# Tyree / Tree
+		if (word == "tyree" or word == "tree") and tyreePosted == "false":
+			tyreePosted = "true"
+			returnURLS.append(PostTyreePic())
 
 	return returnURLS
