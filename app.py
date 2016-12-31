8745913c65276395b65629d4880aca4e3fb43814
Calvin Kopitz <calvinjk@umich.edu>
2016-12-31 17:34:40 -0500

    moving to live

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 def say(s):
 	if (s != "-0"):
 		# USE THIS LINE FOR DEVELOPMENT
-		requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "088ef849ec340699ee3cbeabb8","text": s})
+		#requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "088ef849ec340699ee3cbeabb8","text": s})
 	
 		# USE THIS LINE FOR LIVE
-	  	#requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "2105543276296e8bd2fec9c082","text": s})
+	  	requests.post('https://api.groupme.com/v3/bots/post', data = {"bot_id": "2105543276296e8bd2fec9c082","text": s})
 
 @app.route('/', methods = ['GET', 'POST'])
 def masterRunEverything():
